1eeb85be1b570de6662b1ae3417cbbf0f222836f
Khoshrav <khoshrav@gmail.com>
2025-02-09 11:01:06 -0500

    Refactor files and update readme

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 A prerequisite to the deep learning is to run the matlab code for each subject.
 Then, preprocess the trial wise data using: 
 ```
+cd src/xsens
 python getTrialData.py
 ``` 
-After that, run 
+The above step only needs to be done once unless new participants are analyzed using the matlab code
+
+After that, to train a DL model run 
 ```
+cd src
 python -m ML.train_lstm
 ```
--- src/ML/move_datasets.py
+++ src/ML/move_datasets.py
@@ -1,15 +1,13 @@
-import os
 import torch
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from sklearn import preprocessing
+from random import sample
 
 
 class MovementDataset(Dataset):
 
-    def __init__(self, device, X, y):
+    def __init__(self, device, X, y, label_shuffle_probability):
         self.device = device
         self.X = X
         le = preprocessing.LabelEncoder()
@@ -25,6 +23,9 @@ class MovementDataset(Dataset):
         le.fit(diagnoses)
         self.y = le.transform(diagnoses)
         self.diagnoses_mappings = dict(zip(le.classes_, le.transform(le.classes_)))
+        if label_shuffle_probability > 0.0:
+            unique_labels = np.unique(self.y)
+            self.y = self.labelShuffle(unique_labels, label_shuffle_probability)
         unique, label_counts = np.unique(self.y, return_counts=True)
         dataset_counts = dict(zip(unique, label_counts))
         print(self.diagnoses_mappings)
@@ -52,3 +53,12 @@ class MovementDataset(Dataset):
         ys = torch.from_numpy(ys).float().type(torch.LongTensor).to(device=self.device)
 
         return xs, ys
+
+    def labelShuffle(self, unique_labels, probability):
+        new_y = self.y.copy()
+        for class_label in unique_labels:
+            class_indices = np.where(self.y == class_label)[0]
+            subset_size = int(class_indices.shape[0] * probability)
+            subset_indices = sample(list(class_indices), subset_size)
+            new_y[subset_indices] = np.random.choice(unique_labels, subset_size)
+        return new_y
